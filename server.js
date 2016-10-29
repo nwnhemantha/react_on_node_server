ee1dffb0ac0e2df88449649f80c5988b3d5e9a93
Nuwan Hemantha <nwnhemantha@gmail.com>
2016-10-29 23:54:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+
+// server requests
+app.get('/api/test', (req, res) => {
+  
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
